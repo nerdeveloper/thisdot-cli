436e8203f1f202fc7cf5e67c2562cfcbe6c9ef5f
nerdeveloper <odirionye@gmail.com>
2019-08-11 23:32:28 +0100

    addes linting rules and refactored code

--- src/api.js
+++ src/api.js
@@ -1,64 +1,59 @@
-'use strict';
-
 const number = {};
 const axios = require('axios');
 const chalk = require('chalk');
 
 exports.calculate = async (zipcode, subtotal) => {
-   subtotal = (+subtotal).toFixed(2);
+  subtotal = (+subtotal).toFixed(2);
 
-   await axios.get('https://deft-cove-227620.appspot.com/api/tax', {
-        auth: {
-            username: 'user',
-            password: 'token'
-        },
-        params: {
-            zipcode: zipcode,
-        },
-
-    }).then(response => {
-        if (response.status === 200) {
-
-            number.zipcode = response.data.zipcode;
-            number.tax_rate = response.data.tax_rate;
-            number.tax_total = (number.tax_rate / 100) * subtotal;
-            number.total = +subtotal + number.tax_total;
-        } else {
-
-            console.log(chalk.red(`Error: ${response.status.statusText}`));
-        }
-
-
-
-
-    }).catch(error => {
-        console.log(error);
-    });
-    const params = {
-        zipcode: zipcode,
-        tax_rate: number.tax_rate,
-        sub_total: subtotal,
-        tax_total: number.tax_total.toFixed(2),
-        total: number.total.toFixed(2)
-
-    }
-    let auth = {
+  await axios
+    .get('https://deft-cove-227620.appspot.com/api/tax', {
+      auth: {
         username: 'user',
-        password: 'token'
-    }
-    await axios.post('https://deft-cove-227620.appspot.com/api/order', null, {
-        auth,
-        params
-
-    }).then(response => {
-        if (response.data.status_code === 0) {
-            console.log(chalk.blue.bold(`Message: ${response.data.status_message}`));
-        } else {
-            console.log(chalk.red.bold(`Error: ${response.data.status_message}`));
-        }
-    }).catch(error => {
-        console.log(error);
+        password: 'token',
+      },
+      params: {
+        zipcode,
+      },
+    })
+    .then(response => {
+      if (response.status === 200) {
+        number.zipcode = response.data.zipcode;
+        number.tax_rate = response.data.tax_rate;
+        number.tax_total = (number.tax_rate / 100) * subtotal;
+        number.total = +subtotal + number.tax_total;
+      } else {
+        console.log(chalk.red(`Error: ${response.status.statusText}`));
+      }
+    })
+    .catch(error => {
+      console.log(error);
     });
-
-}
-
+  const params = {
+    zipcode,
+    tax_rate: number.tax_rate,
+    sub_total: subtotal,
+    tax_total: number.tax_total.toFixed(2),
+    total: number.total.toFixed(2),
+  };
+  const auth = {
+    username: 'user',
+    password: 'token',
+  };
+  await axios
+    .post('https://deft-cove-227620.appspot.com/api/order', null, {
+      auth,
+      params,
+    })
+    .then(response => {
+      if (response.data.status_code === 0) {
+        console.log(
+          chalk.blue.bold(`Message: ${response.data.status_message}`),
+        );
+      } else {
+        console.log(chalk.red.bold(`Error: ${response.data.status_message}`));
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 
 const program = require('commander');
 const chalk = require('chalk');
-const api = require('./api')
 const ora = require('ora');
 const boxen = require('boxen');
+const api = require('./api');
+
 program
 
   .description('CLI for checking Tax and Totals')
   .option('-z, --zipcode <type>', 'zip code of your location')
   .option('-s, --subtotal  <number>', 'sub total of the Amount')
-  .version('0.0.1')
+  .version('0.0.1');
 
-
-program.on('--help', function () {
+program.on('--help', function() {
   console.log('');
   console.log('Examples:');
   console.log('');
@@ -23,8 +23,18 @@ program.on('--help', function () {
 program.parse(process.argv);
 
 (async () => {
-  var zipcode = ["20500", "20748", "34248", "37312", "46523",
-    "46523", "75093", "75876", "84111", "95361"];
+  const zipcode = [
+    '20500',
+    '20748',
+    '34248',
+    '37312',
+    '46523',
+    '46523',
+    '75093',
+    '75876',
+    '84111',
+    '95361',
+  ];
 
   if (program.zipcode === undefined && program.subtotal === undefined) {
     program.help();
@@ -32,38 +42,36 @@ program.parse(process.argv);
 
   if (program.zipcode && program.subtotal === undefined) {
     console.log(chalk.red(`error: Enter a valid subtotal`));
-  } else {
-    if (program.subtotal && program.zipcode === undefined) {
-      console.log(chalk.red(`error: Enter a valid zip code`));
-
-    }
+  } else if (program.subtotal && program.zipcode === undefined) {
+    console.log(chalk.red(`error: Enter a valid zip code`));
   }
 
   if (program.subtotal && program.zipcode) {
     const checkifDecmailisPresent = program.subtotal.indexOf('.') < 1;
     if (checkifDecmailisPresent) {
-      console.log(chalk.red(`error: pass in a valid decimal number e.g 10000.00`));
-    }
-    else {
-
-      if (!zipcode.includes(program.zipcode)) {
-        console.log(chalk.red(`error: Enter a valid zipcode that matches your location`));
-
-      }
-      else {
-        const spinner = ora('Submitting Data Inputs').start();
-        setTimeout(() => {
-          spinner.color = 'yellow';
-        }, 1000);
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-        await delay(1000);
-        console.log(boxen(`Order Sub-Total: ${program.subtotal} \nZipcode: ${program.zipcode}`, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue' }));
-        spinner.succeed('Submitted Data Inputs.');
-        console.log(`Hey you! Please Wait ... 🛑🛑🛑`);
-        await api.calculate(program.zipcode, program.subtotal);
-
-      }
+      console.log(
+        chalk.red(`error: pass in a valid decimal number e.g 10000.00`),
+      );
+    } else if (!zipcode.includes(program.zipcode)) {
+      console.log(
+        chalk.red(`error: Enter a valid zipcode that matches your location`),
+      );
+    } else {
+      const spinner = ora('Submitting Data Inputs').start();
+      setTimeout(() => {
+        spinner.color = 'yellow';
+      }, 1000);
+      const delay = ms => new Promise(res => setTimeout(res, ms));
+      await delay(1000);
+      console.log(
+        boxen(
+          `Order Sub-Total: ${program.subtotal} \nZipcode: ${program.zipcode}`,
+          {padding: 1, margin: 1, borderStyle: 'double', borderColor: 'blue'},
+        ),
+      );
+      spinner.succeed('Submitted Data Inputs.');
+      console.log(`Hey you! Please Wait ... 🛑🛑🛑`);
+      await api.calculate(program.zipcode, program.subtotal);
     }
   }
-})
-  ();
+})();
