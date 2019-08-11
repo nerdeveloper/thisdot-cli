ecff40ce49149d759e16fdbf1895abde7a110190
nerdeveloper <odirionye@gmail.com>
2019-08-11 16:21:03 +0100

    added support options for CLI

--- src/api.js
+++ src/api.js
@@ -2,8 +2,10 @@
 
 const number = {};
 const axios = require('axios');
+const chalk = require('chalk');
 
-export const calculate = async (zipcode, subtotal) => {
+exports.calculate = async (zipcode, subtotal) => {
+    subtotal = (+subtotal).toFixed(2);
 
     await axios.get('https://deft-cove-227620.appspot.com/api/tax', {
         auth: {
@@ -18,13 +20,12 @@ export const calculate = async (zipcode, subtotal) => {
         number.zipcode = response.data.zipcode;
         number.tax_rate = response.data.tax_rate;
         number.tax_total = (number.tax_rate / 100) * subtotal;
-        console.log(typeof subtotal, typeof number.tax_total);
         number.total = +subtotal + number.tax_total;
+        
 
  }).catch(error => {
         console.log(error);
     });
-    await console.log(number.total)
     const params = {
         zipcode: zipcode,
         tax_rate: number.tax_rate,
@@ -43,9 +44,9 @@ export const calculate = async (zipcode, subtotal) => {
 
     }).then(response => {
         if (response.data.status_code === 0) {
-            console.log(response.data.status_message);
+            console.log(chalk.blue(`Message: ${response.data.status_message}`));
         } else {
-            console.log(response.data.status_message);
+            console.log(chalk.red(`Error: ${response.data.status_message}`));
         }
  }).catch(error => {
         console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const chalk = require('chalk');
+const api = require('./api')
 program
     
     .description('CLI for checking Tax and Totals')
-    .option('-z, --zipcode', 'zip code of your location')
-    .option('-s, --subtotal', 'sub total of the the Amount')
-    .version('1.0.0')
+    .option('-z, --zipcode <type>', 'zip code of your location')
+    .option('-s, --subtotal  <type>', 'sub total of the the Amount')
+    .version('0.0.1')
 
-    
-  
+
+    program.on('--help', function(){
+        console.log('');
+        console.log('Examples:');
+        console.log('');
+        console.log('  $ thisdot-cli --zipcode 20500 --subtotal 100000.00');
+      });
+
 program.parse(process.argv);
-
-if (!program.args.length) program.help();
